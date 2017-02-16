aa1854a189e5364672ecb6e02e3325a7c51aeab6
Paul <pawi552@gmail.com>
2017-02-16 14:35:02 +0000

    bugfix

--- sub_server.py
+++ sub_server.py
@@ -7,67 +7,58 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from time import sleep
-#email = webdriver.Firefox()
 
 print(str(sys.argv))
 
 browser = webdriver.Firefox()
-browser.get("http://tellsubway.ie/")
-#email.get("http://10minutemail.com/")
 sleep(3)
+browser.get("http://tellsubway.ie/")
 
 elem = browser.find_element_by_id('txtSearch') # Find the search box
 elem.send_keys(str(sys.argv[5]))
 browser.find_element_by_xpath(".//div[@class='TS_btn1']").click()
 sleep(5)
 browser.find_element_by_id('agree').click()
-sleep(3)
-date_elem=browser.find_element_by_id('answ4304')
+sleep(5)
+date_elem=browser.find_element_by_id('date_10102')
 date_elem.send_keys(str(sys.argv[1]))
-hour_elem = browser.find_element_by_id('answHour4304')
+hour_elem = browser.find_element_by_id('hour_10102')
 hour_elem.send_keys(str(sys.argv[2]))
-minutes_elem=browser.find_element_by_id('answMinute4304')
+minutes_elem=browser.find_element_by_id('minute_10102')
 minutes_elem.send_keys(str(sys.argv[3]))
-browser.find_element_by_id('answc430610').click()
-browser.find_element_by_id('answ16472Q4307').click()
+browser.find_element_by_xpath(".//div[@class='StepBtn']").click()
 
-browser.find_element_by_id('answ16484C4309Q4308').click()
-browser.find_element_by_id('answ16495C4310Q4308').click()
-browser.find_element_by_id('answ16506C4311Q4308').click()
-browser.find_element_by_id('answ16517C4312Q4308').click()
-browser.find_element_by_id('answ16528C4313Q4308').click()
-browser.find_element_by_id('answ16539C4314Q4308').click()
+browser.find_element_by_id('opt_10').click()
+browser.find_element_by_id('opt_49799').click()
+browser.find_element_by_id('opt_49811').click()
+browser.find_element_by_id('opt_49822').click()
+browser.find_element_by_id('opt_49833').click()
+browser.find_element_by_id('opt_49844').click()
+browser.find_element_by_id('opt_49855').click()
+browser.find_element_by_id('opt_49866').click()
 
-q1 = Select(browser.find_element_by_xpath(".//select[@id='answ4327']"))
+sleep(5)
 
-q1.select_by_visible_text("No")
+browser.find_element_by_xpath('//*[@id="page_2"]/div[2]/a[1]/div').click()
 
-q2 = Select(browser.find_element_by_xpath(".//select[@id='answ4329']"))
-
-q2.select_by_visible_text("No")
-
-q3 = Select(browser.find_element_by_xpath(".//select[@id='answ4331']"))
-
+browser.find_element_by_xpath('//*[@id="question_10124"]/div/div/label[2]').click()
+browser.find_element_by_xpath('//*[@id="question_10126"]/div/div/label[2]').click()
+q3 = Select(browser.find_element_by_xpath('//*[@id="10128"]'))
 q3.select_by_visible_text("2")
-
-q4 = Select(browser.find_element_by_xpath(".//select[@id='answ4332']"))
-
+q4 = Select(browser.find_element_by_xpath('//*[@id="10129"]'))
 q4.select_by_visible_text("2")
 
-email_elem = browser.find_element_by_id('answ4333')
+browser.find_element_by_xpath('//*[@id="page_3"]/div[2]/a[1]/div').click()
 
+
+email_elem = browser.find_element_by_xpath('//*[@id="10130"]')
 email_elem.send_keys(str(sys.argv[4]))
+browser.find_element_by_xpath('//*[@id="question_10131"]/div/div/label[2]').click()
+browser.find_element_by_xpath('//*[@id="question_contact_me"]/div/div/label[2]').click()
 
-q5 = Select(browser.find_element_by_xpath(".//select[@id='answ4334']"))
-
-q5.select_by_visible_text("No")
-
-q6 = Select(browser.find_element_by_xpath(".//select[@id='DdlContact']"))
-
-q6.select_by_visible_text("No")
 sleep(3)
+browser.find_element_by_xpath('//*[@id="page_4"]/div[2]/a[1]/div').click()
 
-browser.find_element_by_id('btnSubmit').click()
 sleep(9)
 
 try:
@@ -81,7 +72,7 @@ if err:
 	print("Can't get a cookie")
 	browser.close()
 	sleep(3)
-	exit(0)
+	exit(1)
 else:
 	print("cookie send to "+str(sys.argv[4]))
 	browser.close()
